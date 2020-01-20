1a97f17af5ef6c93a1a103bdd456cc4429f5d4ee
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-01-20 08:51:05 +0100

    Fixing the ign-Up route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './component/SignIn/SignIn';
+import SignUp from './component/SignUp/SignUp';
 import './reset.css';
 import 'tachyons';
 import './App.css';
 
+
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
 
-      <SignIn />
-      <h2>Glad to be back again</h2>
-    </div>
+          <h2>Glad to be back again</h2>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
