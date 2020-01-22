fc72a06676e9c5f2e5bc818f8eb1fbce37d234ca
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-01-22 04:33:13 +0100

    webpack issue on https

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <nav className="db dt-l w-100 border-box pa3 ph5-l navbar">
             <Link className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l" href="/" title="Home">
-                <img src={teamwork} className="dib w4" alt="Teamwork" />
+                <img src={teamwork} className="dib w4" alt="Teamwork Fan" />
             </Link>
             <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
                 <Link className="link dim dark-gray f6 f5-l dib mr3 mr4-l" to="/" title="Home">Home</Link>
