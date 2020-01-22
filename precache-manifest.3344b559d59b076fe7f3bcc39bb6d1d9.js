cb409130bbcadd9bbc8ae9ae126f9f087f3a69f5
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-01-22 03:15:53 +0100

    Updates

---
+++ precache-manifest.3344b559d59b076fe7f3bcc39bb6d1d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c10210b62c9f0abba0493971db4ea5",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "6e47c1f43780d967c801",
+    "url": "/deploy/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b8c58aec990886025129",
+    "url": "/deploy/static/css/main.dee7dc1f.chunk.css"
+  },
+  {
+    "revision": "6e47c1f43780d967c801",
+    "url": "/deploy/static/js/2.3af6b4ce.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/deploy/static/js/2.3af6b4ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8c58aec990886025129",
+    "url": "/deploy/static/js/main.b999729d.chunk.js"
+  },
+  {
+    "revision": "cdbadde63e82e25c85e2",
+    "url": "/deploy/static/js/runtime-main.6e3e3f2e.js"
+  },
+  {
+    "revision": "9315804b93ebdc18a751eb251b2e851c",
+    "url": "/deploy/static/media/colabo.9315804b.png"
+  },
+  {
+    "revision": "018a247448aeb2cb8be8c4d38fefd059",
+    "url": "/deploy/static/media/communicate.018a2474.png"
+  },
+  {
+    "revision": "35cfc5bf83ec956c988546a66721c58b",
+    "url": "/deploy/static/media/error.35cfc5bf.png"
+  },
+  {
+    "revision": "9cd3b33b00d0785cc358a93f8ae790c2",
+    "url": "/deploy/static/media/network.9cd3b33b.png"
+  },
+  {
+    "revision": "2b630837a48a05130e2009f37c03269e",
+    "url": "/deploy/static/media/social.2b630837.png"
+  },
+  {
+    "revision": "de0ecb8621affc9ddf98da1f6c2078c2",
+    "url": "/deploy/static/media/team.de0ecb86.png"
+  },
+  {
+    "revision": "9e90d7797e50aa490242f91d273f6e8e",
+    "url": "/deploy/static/media/teamwork.9e90d779.png"
+  }
+]);
